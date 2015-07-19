7d9635caa12e2fe56cbfc432199faf25222cac75
zhang <tengjinfly@hotmail.com>
2015-07-20 00:09:03 +0800

    添加js工具
    添加js日期操作方法，待续。。

---
+++ Garlic.Web/JSTools/DateTimeHelper.js
@@ -0,0 +1,36 @@
+﻿
+
+
+
+
+//获取今天日期2015-07-18形式
+function GetDate() {
+    var mydate = new Date();
+    var str = "" + mydate.getFullYear() + "-";
+    str += (mydate.getMonth() + 1) + "-";
+    str += mydate.getDate();
+    return str
+}
+
+//获取昨天日期2015-07-17形式
+function GetYesDayDate() {
+    var oneday = 1000 * 60 * 60 * 24;
+    var today = new Date();
+    var yesday = new Date(today - oneday);
+    var str = "" + yesday.getFullYear() + "-";
+    str += (yesday.getMonth() + 1) + "-";
+    str += yesday.getDate();
+    return str
+}
+
+//获取指定一天的前一天 2015-07-15形式
+function GetYesterday(mydate) {
+    var oneday = 1000 * 60 * 60 * 24;
+    var date = new Date(mydate);
+    var yesday = new Date(date - oneday);
+    var str = "" + yesday.getFullYear() + "-";
+    str += (yesday.getMonth() + 1) + "-";
+    str += yesday.getDate();
+    return str
+}
+
